c90bc484832d05819855ea47094e40b01bcef799
Lisa Malenfant <lisa.malenfant@uci.edu>
2018-02-18 11:51:04 -0800

    Added the WhiteList configuration and missing WhiteList.cs file

---
+++ Vts.Gui.Wpf/ViewModel/Application/WhiteList.cs
@@ -0,0 +1,98 @@
+﻿#if WHITELIST
+namespace Vts.Gui.Wpf.ViewModel.Application
+{
+    public static class WhiteList
+    {
+        public static ForwardSolverType[] ForwardSolverTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    ForwardSolverType.PointSourceSDA,
+                    ForwardSolverType.DistributedPointSourceSDA,
+                    ForwardSolverType.DistributedGaussianSourceSDA,
+                    ForwardSolverType.MonteCarlo,
+                    ForwardSolverType.Nurbs,
+                    ForwardSolverType.TwoLayerSDA, 
+                };
+            }
+        }
+
+        public static ForwardSolverType[] InverseForwardSolverTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    ForwardSolverType.PointSourceSDA,
+                    ForwardSolverType.DistributedPointSourceSDA,
+                    ForwardSolverType.DistributedGaussianSourceSDA,
+                    ForwardSolverType.MonteCarlo,
+                    ForwardSolverType.Nurbs,
+                    //ForwardSolverType.TwoLayerSDA, 
+                };
+            }
+        }
+
+        public static ScatteringType[] ScatteringTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    ScatteringType.PowerLaw,
+                    ScatteringType.Intralipid
+                };
+            }
+        }
+
+        public static RandomNumberGeneratorType[] RandomNumberGeneratorTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    RandomNumberGeneratorType.MersenneTwister
+                };
+            }
+        }
+
+        public static AbsorptionWeightingType[] AbsorptionWeightingTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    AbsorptionWeightingType.Analog,
+                    AbsorptionWeightingType.Discrete,
+                    AbsorptionWeightingType.Continuous
+                };
+            }
+        }
+
+        public static PhaseFunctionType[] PhaseFunctionTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    PhaseFunctionType.HenyeyGreenstein
+                };
+            }
+        }
+
+        public static string[] TissueTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    "MultiLayer",
+                    "SingleEllipsoid",
+                };
+            }
+        }
+    }
+}
+#endif
