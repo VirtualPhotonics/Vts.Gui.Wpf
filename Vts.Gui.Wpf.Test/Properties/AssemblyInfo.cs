5f3a667b44414df53b6c86ccb324f70d457838a6
Lisa Malenfant <lisa.malenfant@uci.edu>
2018-05-16 18:21:06 -0700

    Updated the .NET Framework to 4.6.1 Updated NuGet packages Added a test project

---
+++ Vts.Gui.Wpf.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vts.Gui.Wpf.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vts.Gui.Wpf.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51a96a1e-b043-46e1-92d2-f1cdfaf0647b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Vts.Gui.Wpf.Test/ViewModel/Panels/MonteCarlo/MonteCarloSolverViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Vts.Gui.Wpf.Test.ViewModel.Panels.MonteCarlo
+{
+    /// <summary>
+    /// Summary description for MonteCarloSolverViewModelTests
+    /// </summary>
+    [TestFixture]
+    public class MonteCarloSolverViewModelTests
+    {
+        public MonteCarloSolverViewModelTests()
+        {
+            // constructor logic if needed goes here
+        }
+
+        /// <summary>
+        /// setup and tear down
+        /// </summary>
+        [OneTimeSetUp]
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+        }
+
+        [Test]
+        public void MyUnitTest()
+        {
+
+        }
+    }
+}
