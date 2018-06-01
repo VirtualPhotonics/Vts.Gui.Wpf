09a86e522e919bb85af53074624ee5664f66ed2d
hayakawa <hayakawa@uci.edu>
2018-05-31 18:37:43 -0700

    InverseSolver panel: Percent error of converged results now output to textbox.  Updated unit test to accommodate new output.

--- Vts.Gui.Wpf/ViewModel/Panels/InverseSolverViewModel.cs
+++ Vts.Gui.Wpf/ViewModel/Panels/InverseSolverViewModel.cs
@@ -494,6 +494,21 @@ namespace Vts.Gui.Wpf.ViewModel
                                                                                     MeasuredOpticalPropertyVM + " \r");
                 WindowViewModel.Current.TextOutputVM.TextOutput_PostMessage.Execute("   At Converged Values: " +
                                                                                     ResultOpticalPropertyVM + " \r");
+                                //Display Percent Error
+                double muaError = 0.0;
+                double muspError = 0.0;
+                if (MeasuredOpticalPropertyVM.Mua > 0)
+                {
+                    int tempMuaError = (int)(10000.0 * Math.Abs(ResultOpticalPropertyVM.Mua - MeasuredOpticalPropertyVM.Mua) / MeasuredOpticalPropertyVM.Mua);
+                    muaError = tempMuaError / 100.0;
+                }
+                if (MeasuredOpticalPropertyVM.Musp > 0)
+                {
+                    int tempMuspError = (int)(10000.0 * Math.Abs(ResultOpticalPropertyVM.Musp - MeasuredOpticalPropertyVM.Musp) / MeasuredOpticalPropertyVM.Musp);
+                    muspError = tempMuspError / 100.0;
+                }
+                WindowViewModel.Current.TextOutputVM.TextOutput_PostMessage.Execute("   Computed Percent Error: " + "μa = " + muaError +
+                                                                                    "% μs' Error = " + muspError + "% \r");
             }
 
             var axesLabels = GetPlotLabels();
